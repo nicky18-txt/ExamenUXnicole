c9e0e21d9b65d3cc57334e6d2f74b7f963da955e
nicky18-txt <nic.altamirano@unitec.edu>
2026-03-23 18:55:40 -0600

    Se creo el swagger

--- examen-API/app.js
+++ examen-API/app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var swaggerSetup = require('./config/swagger');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+swaggerSetup(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
